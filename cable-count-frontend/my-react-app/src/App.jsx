44fa486204a4869f562a02447e23d9c3bb96f281
Sparsh N <sparsh.nair@hotmail.com>
2023-06-15 23:45:06 +0530

    Upload full react frontend so far, including node modules

---
+++ cable-count-frontend/my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import Header from './Header'
+import ReactDOM from 'react-dom/client'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+// import { Fragment } from 'react';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+
+  return (
+    <>
+    
+      <p>
+        Hello World
+      </p>
+    </>
+  )
+}
