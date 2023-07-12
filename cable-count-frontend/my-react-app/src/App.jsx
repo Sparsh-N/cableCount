cdf96679366457cf6b95af6a6c6d0ea67a44e769
Sparsh N <107741753+Sparsh-N@users.noreply.github.com>
2023-07-12 23:05:15 +0530

    updated the webpage so elements are aligned

--- cable-count-frontend/my-react-app/src/App.jsx
+++ cable-count-frontend/my-react-app/src/App.jsx
@@ -1,42 +1,67 @@
-import * as React from 'react';
+import React from 'react';
 import './App.css';
-import Header from './PageElements/Header';
 import 'antd/dist/reset.css';
-import { Menu } from "antd";
-// import { Route, Routes, useNavigate } from "react-router"
-import Sidebar from './PageElements/Sidebar';
-import { CssBaseline } from '@mui/material';
+import { Menu } from 'antd';
+import Header from './PageElements/Header';
 
-export default function App() {
-  // const navigate = useNavigate()
+function App() {
   return (
-    <React.Fragment>
-    {/* <CssBaseline /> */}
-    <Header />
-    {/* Sidebar */}
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <Menu
-      onClick={({key})=>{
-        navigate(key)
-      }}
-      items={[
-        {label: "Home"},
-        {label: "Search Cable"},
-        {label: "Add Cable"}
-      ]}></Menu>
-      {/* <Content />  */}
-    </div>
-    {/* Inner Container */}
-    </React.Fragment>
+  <>
+  <Header />
+  <div style={{display:'flex', flexDirection:'row', justifyContent:'left', alignContent:"flex-start"}}>
+    <Menu items={[
+      {label:"Home"},
+      {label:"Add"},
+      {label:"Search"}]}>
+    </Menu>
+    <div>Content</div>
+  </div>
+  </>
   );
 }
 
-// function Content() {
-//   return <div>
-//     <Routes>
-//       <Route path="/" element={<div>Home</div>}></Route>
-//       <Route path="/search" element={<div>Search</div>}></Route>
-//       <Route path="/add" element={<div>Add</div>}></Route>
-//     </Routes>
-//   </div>
+export default App;
+
+
+
+// // import { Route, Routes, useNavigate } from "react-router"
+// 
+// import { CssBaseline } from '@mui/material';
+
+// export default function App() {
+//   // const navigate = useNavigate()
+//   return (
+//     <React.Fragment>
+//     {/* <CssBaseline /> */}
+//     <Header />
+//     {/* Sidebar */}
+//     <div style={{ display: "flex", flexDirection: "column" }}>
+//       <Menu
+//       onClick={({key})=>{
+//         navigate(key)
+//       }}
+//       items={[
+//         {label: "Home", key: "1"},
+//         {label: "Search Cable", key: "2"},
+//         {label: "Add Cable", key: "3"}
+//       ]}></Menu>
+//       <Menu.Item key="1"> <Link to={`${this.props.match.url}/`}> Search </Link> </Menu.Item>
+//       <Menu.Item key="2"> <Link to={`${this.props.match.url}/search`}> Search </Link> </Menu.Item>
+//       <Menu.Item key="3"> <Link to={`${this.props.match.url}/add`}> Add </Link> </Menu.Item>
+
+//       {/* <Content />  */}
+//     </div>
+//     {/* Inner Container */}
+//     </React.Fragment>
+//   );
 // }
+
+// // function Content() {
+// //   return <div>
+// //     <Routes>
+// //       <Route path="/" element={<div>Home</div>}></Route>
+// //       <Route path="/search" element={<div>Search</div>}></Route>
+// //       <Route path="/add" element={<div>Add</div>}></Route>
+// //     </Routes>
+// //   </div>
+// // }
